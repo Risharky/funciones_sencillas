5ddc93eb118458caa81ce39220d74e798ee2a364
Ricardo Rodriguez Otero <70866489+Risharky@users.noreply.github.com>
2026-05-15 15:41:39 -0500

    dand opcionede impresion

--- conversiones_base.py
+++ conversiones_base.py
@@ -3,12 +3,12 @@
 
 #convertir entero
 num="713"
-int(num)*10
+print(int(num)*10)
 
 #convertir en float
 num2="713"
-float(num2)-13.7
+print(float(num2)-13.7)
 
 #convertir a texto 
 num3=713
-"Este numero el " +str(num3)+ "  es un número aleatorio"
+print("Este numero el " +str(num3)+ "  es un número aleatorio")
