ea9c8d63b041d673e880a5e3993c86a0ac5bb3a1
Ricardo Rodriguez Otero <70866489+Risharky@users.noreply.github.com>
2026-05-15 15:39:16 -0500

    conversiones sencillas

---
+++ conversiones_base.py
@@ -0,0 +1,14 @@
+#funciones de fabrica
+# python
+
+#convertir entero
+num="713"
+int(num)*10
+
+#convertir en float
+num2="713"
+float(num2)-13.7
+
+#convertir a texto 
+num3=713
+"Este numero el " +str(num3)+ "  es un número aleatorio"
